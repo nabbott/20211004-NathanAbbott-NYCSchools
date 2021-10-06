b2e5b6f938bb35661afbb2a09b2f8c2ee7170719
Nate <rmtectrlr@hotmail.com>
2021-10-06 19:55:14 -0400

    Added an activity indicator and failure indicators to the web view

--- NYCSchools/View Controllers/WebsiteViewController.swift
+++ NYCSchools/View Controllers/WebsiteViewController.swift
@@ -12,18 +12,48 @@ import WebKit
 
 class WebsiteViewController: UIViewController {
     var website:String?
+    var school:String?
     
-    @IBOutlet weak var webkitView:WKWebView!
+    let maxLoadAttempts=2
+    lazy var loadAttempts=maxLoadAttempts
+    
+    @IBOutlet weak var activityIndicatorView:UIView!
+    @IBOutlet weak var actitityIndicator:UIActivityIndicatorView!
+    @IBOutlet weak var activityLabel:UILabel!
+    @IBOutlet weak var webkitView:WKWebView! {
+        didSet {
+            webkitView.navigationDelegate=self
+        }
+    }
     
     override
     func viewDidLoad() {
+        loadWebsite()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        loadAttempts=maxLoadAttempts
+        
+        navigationController?.setToolbarHidden(true, animated: animated)
+        actitityIndicator.startAnimating()
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        navigationController?.setToolbarHidden(false, animated: animated)
+    }
+    
+    func loadWebsite(useHTTPS:Bool=true){
+        guard loadAttempts>0 else {
+            os_log(.debug,"Load attempt limit exceeded")
+            return
+        }
+        
         guard var ws=website else {
             return
         }
         
-        
         if !ws.starts(with: "http") {
-            ws="https://\(ws)"
+            ws="http\(useHTTPS ? "s":"")://\(ws)"
         }
         
         //FIXME: Occasionally websites fail to load, possible because of a malformed url
@@ -32,14 +62,54 @@ class WebsiteViewController: UIViewController {
         let url = URL(string:ws)
         let req = URLRequest(url: url!)
         webkitView.load(req)
-    }
-    
-    override func viewWillAppear(_ animated: Bool) {
-        navigationController?.setToolbarHidden(true, animated: animated)
-    }
-    
-    override func viewWillDisappear(_ animated: Bool) {
-        navigationController?.setToolbarHidden(false, animated: animated)
+        
+        loadAttempts -= 1
     }
 }
 
+extension WebsiteViewController:WKNavigationDelegate {
+    func webView(_ webView: WKWebView, didCommit: WKNavigation!){
+        os_log(.error, "Website: %@ did commit",website!)
+    }
+    
+    func webView(_ webView: WKWebView, didFinish: WKNavigation!){
+        os_log(.error, "Website: %@ finished loading",website!)
+        
+        actitityIndicator.stopAnimating()
+        UIView.animate(withDuration: 5){[unowned self] in
+            self.activityIndicatorView.isHidden=true
+            self.webkitView.isHidden=false
+        }
+    }
+    
+    //MARK: - Load Failures
+    
+    func handleLoadFail(){
+        if loadAttempts > 0 {
+            if let ws=website {
+                os_log(.debug,"Attempting to load %@ over http",ws)
+                loadWebsite(useHTTPS: false)
+            }
+            return
+        }
+        
+        actitityIndicator.stopAnimating()
+        activityLabel.text="Could not load \(school ?? "") website"
+        
+        UIView.animate(withDuration: 2){[unowned self] in
+            self.actitityIndicator.isHidden=true
+            self.activityIndicatorView.isHidden=false
+            self.webkitView.isHidden=true
+        }
+    }
+    
+    func webView(_ webView: WKWebView, didFail: WKNavigation!, withError: Error){
+        os_log(.error, "Website: %@ load failed with error: %@",website!,withError as NSError)
+        handleLoadFail()
+    }
+    
+    func webView(_ webView: WKWebView, didFailProvisionalNavigation: WKNavigation!, withError: Error){
+        os_log(.error, "Website: %@ provisional load failed with error: %@",website!,withError as NSError)
+        handleLoadFail()
+    }
+}
